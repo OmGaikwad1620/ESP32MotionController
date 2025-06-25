7613786f870f934be65b1b65d1829882fa213bc9
Om <iamomgaikwad16@gmail.com>
2025-06-25 21:54:49 +0530

    Create MotionControllerV1.ino

---
+++ MotionControllerV1.ino
@@ -0,0 +1,13 @@
+#include <Wire.h>
+#include <MPU6050.h>
+#include <Arduino.h>
+#include <BleGamepad.h>
+
+BleGamepad wiix;
+
+MPU6050 mpu;//SCL(GPIO22),SDA(GPIO21).
+
+void setup(){
+}
+void loop() {
+}
